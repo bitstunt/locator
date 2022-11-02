2de0cf26a48b13792376e0d6d7c5973a246192f8
Harsh Ranjan Sinha <harshranjansinha@outlook.in>
2022-11-02 17:22:11 +0530

    almost there

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
-function Index (props) {
-    const [visibility,setVisibility] = useState(true)
+//stylesheets
+import '../stylesheets/Index.css';
+
+function Index(props) {
     let options = ['hello', 'hello again', 'once more']
     let filterOption = (options, query) => {
-        if(!query)
-        return [];
-        
+        if (!query)
+            return [];
+
         query = query.toLowerCase()
         return options.filter((e) => {
             return e.includes(query)
         })
     }
     let result = filterOption(options, props.query);
-    let option = result.map((e,i) => {return (
-    <button onClick={() =>{
-        props.setQuery(e)
-    setVisibility(false)}
-    } key={i}>{e}</button>
-    )})
-    return(
-        <div className="iContainer">
+    let option = result.map((e, i) => {
+        return (
+            <button className="ibutton" onClick={() => {props.setFocused(false); props.setQuery(e);}} key={i}>{e}</button>
+        )
+    })
+    return (
+        <div className={(props.focused) ? 'iContainer' : 'iContainer invisible'}>
             {option}
         </div>
     );
